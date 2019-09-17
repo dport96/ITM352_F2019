53d998765f612868e725c1254947ef137b7961c0
Rick Kazman <kazman@hawaii.edu>
2019-09-16 21:36:51 -1000

    Create Ex4.js

---
+++ Lab4/Ex4.js
@@ -0,0 +1,15 @@
+var http = require('http');
+
+//create a server object:
+http.createServer(function (req, res) {
+    console.log(req.headers); //output the request headers to the console
+    res.writeHead(200, { 'Content-Type': 'text/html' }); // set MIME type to HTML 
+    //res.writeHead(301, { "Location": "http://google.com"}); 
+   // res.write('<script>window.location = "http://www.google.com";</script>'); //write a response to the client
+    res.write('<META http-equiv ="refresh" content="0;URL=http://www.google.com">');
+    res.end(); //end the response
+  }).listen(8080); //the server object listens on port 8080
+  
+  console.log('Hello world HTTP server listening on localhost port 8080');
+  
+   
