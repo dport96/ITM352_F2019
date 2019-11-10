37c0607a6b64528b082798cc90f1039e5f793500
rnkazman <kazman@hawaii.edu>
2019-11-09 23:01:24 -1000

    Update info_server_Ex2.js

--- Lab13/info_server_Ex2.js
+++ Lab13/info_server_Ex2.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var myParser = require("body-parser");
 
+// Function to test if a string is a non-negative integer
 function isNonNegInt(q, returnErrors = false) {
     errors = []; // assume no errors at first
     if (Number(q) != q) errors.push('Not a number!'); // Check if string is a number value
@@ -10,16 +11,19 @@ function isNonNegInt(q, returnErrors = false) {
     return returnErrors ? errors : (errors.length == 0);
 }
 
+// Initialize Express
 app.all('*', function (request, response, next) {
     console.log(request.method + ' to ' + request.path);
     next();
 });
 
 app.use(myParser.urlencoded({ extended: true }));
+
+// Set up the path and handler for POST requests
 app.post("/process_form", function (request, response) {
     let POST = request.body;
-    //response.send(POST);
 
+    // Check the quantity entered by the user
     if (typeof POST['quantity_textbox'] != 'undefined') {
         q = POST['quantity_textbox'];
         if (isNonNegInt(q)) {
@@ -32,5 +36,6 @@ app.post("/process_form", function (request, response) {
 
 });
 
+// Look for files in the "public" folder and listen on port 8080
 app.use(express.static('./public'));
 app.listen(8080, () => console.log(`listening on port 8080`));
